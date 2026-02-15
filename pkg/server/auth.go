f9df7c8fbd6f545557cf3acdaea8c5729c5d4397
Elly <145195760+ellypaws@users.noreply.github.com>
2026-02-15 19:59:49 +0800

    feat(auth): include resolved avatar and banner URLs in JWT claims
    - add `Banner` field to `JwtCustomClaims` struct

--- pkg/server/auth.go
+++ pkg/server/auth.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,6 +18,7 @@ type JwtCustomClaims struct {
 	UserID   string            `json:"uid"`
 	Username string            `json:"sub"`
 	Avatar   string            `json:"avt"`
+	Banner   string            `json:"ban"`
 	IsAdmin  bool              `json:"adm"`
 	Roles    []*discordgo.Role `json:"roles"`
 	jwt.RegisteredClaims
@@ -30,10 +33,14 @@ func GetJWTSecret() []byte {
 }
 
 func GenerateToken(user *types.User, roles []*discordgo.Role) (string, error) {
+	avatarURL := generateAvatarURL(user.UserID, user.Avatar)
+	bannerURL := generateBannerURL(user.UserID, user.Banner)
+
 	claims := &JwtCustomClaims{
 		UserID:   user.UserID,
 		Username: user.Username,
-		Avatar:   user.Avatar,
+		Avatar:   avatarURL,
+		Banner:   bannerURL,
 		IsAdmin:  user.IsAdmin,
 		Roles:    roles,
 		RegisteredClaims: jwt.RegisteredClaims{
@@ -44,6 +51,36 @@ func GenerateToken(user *types.User, roles []*discordgo.Role) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(GetJWTSecret())
+}
+
+func generateAvatarURL(userID, avatarHash string) string {
+	if avatarHash == "" {
+		// Generate default avatar URL based on user ID discriminator
+		// Discord uses (user_id >> 22) % 6 for default avatar index
+		discriminator := 0
+		if len(userID) > 0 {
+			for _, c := range userID {
+				discriminator = (discriminator*10 + int(c-'0')) % 6
+			}
+		}
+		return fmt.Sprintf("https://cdn.discordapp.com/embed/avatars/%d.png", discriminator)
+	}
+
+	if strings.HasPrefix(avatarHash, "a_") {
+		return fmt.Sprintf("https://cdn.discordapp.com/avatars/%s/%s.gif", userID, avatarHash)
+	}
+	return fmt.Sprintf("https://cdn.discordapp.com/avatars/%s/%s.png", userID, avatarHash)
+}
+
+func generateBannerURL(userID, bannerHash string) string {
+	if bannerHash == "" {
+		return ""
+	}
+
+	if strings.HasPrefix(bannerHash, "a_") {
+		return fmt.Sprintf("https://cdn.discordapp.com/banners/%s/%s.gif", userID, bannerHash)
+	}
+	return fmt.Sprintf("https://cdn.discordapp.com/banners/%s/%s.png", userID, bannerHash)
 }
 
 // GetUserFromContext extracts claims from the echo context (set by middleware)
